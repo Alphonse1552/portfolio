0349aea92083e77d75f306df4521752eea13cf21
Alphonse1552 <72935508+Alphonse1552@users.noreply.github.com>
2021-06-13 14:35:06 -0400

    edited things

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,103 @@
-import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import React, { useState } from "react"
+import { graphql, useStaticQuery } from 'gatsby'
+import "./layout.css"
 
-
-const Header = ({ siteTitle }) => (
+const Header = () => (
 
   <header
+  
     style={{
       background: `#293b5f`,
-      marginBottom: `1.45rem`,
+      marginBottom: `1.25rem`,
+      
     }}
   >
+    
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        maxWidth: '0 auto',
+        padding: `1.25rem 1.0875rem`,
+        display: 'flex',
+        
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <div>
+        
+      <h1 style={{}}>
         <Link
           to="/"
           style={{
-            color: `black`,
+            textAlign: 'center',
             textDecoration: `blink`,
           }}
         >
+          
           <font color="dbe6fd"> 
-          {siteTitle}
+       
+          Michael Moschello
+        
           </font>
         </Link>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       </h1>
+      </div>
+
+
+ 
+       <font color="dbe6fd"> 
+       
+        <h3>
+
+        <Link
+        
+          to="/about"
+          style={{
+            textDecoration: `blink`,
+            textAlign: 'center',
+          }}
+        >
+          <font color="dbe6fd"> 
+          About
+          </font>
+        </Link>
+        &nbsp;  |  &nbsp;
+   
+        <Link
+          to="/engineering"
+          style={{
+            textAlign: 'center',
+            textDecoration: `blink`,
+          }}
+        >
+         
+          <font color="dbe6fd"> 
+          Engineering
+          </font>
+        </Link>
+        
+        &nbsp;  |  &nbsp;
+      
+      
+        <Link
+          to="/debate"
+          style={{
+            textAlign: 'center',
+            textDecoration: `blink`,  
+          }}
+        >
+          <font color="dbe6fd"> 
+          Debate
+          </font>
+        </Link>
+        
+        </h3>
+      
+        </font>   
     </div>
+  
   </header>
 )
 
