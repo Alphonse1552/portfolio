36aae410cefe6afc49754249aacd8742f4efa288
Alphonse1552 <72935508+Alphonse1552@users.noreply.github.com>
2021-06-14 00:22:21 -0400

    ok this is version 1.0

--- src/pages/debate.js
+++ src/pages/debate.js
@@ -33,6 +33,7 @@ const Blog = () => {
         <font color="dbe6fd">  
         <h3>Besides building and engineering things, I also am the Secretary Treasurer of Ridge Forensics and a national champion Congressional Debater </h3>
         </font> 
+      <font color="#0000"> 
       <div className = 'box'>
       <h2>2021 Season</h2>
         <h3>My current debate record with all of the tournaments that I have attended thus far in the 2021 season</h3>
@@ -48,6 +49,7 @@ const Blog = () => {
         <h4>Here is the document I used outlining all of my speeches and refutations I made during the tournament</h4>
       <iframe src="https://docs.google.com/document/d/e/2PACX-1vTK6Q7gV8jO34rPjhTGSYWnG43vT3ljvazCkr1ITzvKz2NnyapCwJNwD_H4dDIN89rS2aLd3eAWlvW1/pub?embedded=true" width="600" height="400" ></iframe>
         </div>
+        </font>
         </center>
         <h5>I am currently working out the kinks with having things resize to different screen sizes in regards to the embeds </h5>
     </Layout>
